84c02ab63c5937511fc06aa2fbe773e10557fb83
rkappler <43675242+rkappler@users.noreply.github.com>
2019-03-08 15:52:36 +0100

    Create Get.java

---
+++ src/main/java/com/example/demo/controller/Get.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Get {
+
+    @GetMapping("/get/{id}")
+    @ResponseBody
+    public String get(@PathVariable String id){
+
+        return id;
+
+    }
+
+}
